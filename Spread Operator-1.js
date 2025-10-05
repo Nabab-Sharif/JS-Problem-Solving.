0a68851a0ece2ba561605d856e79a9ef598dff8b
Nabab-Sharif <mdnababsharif2@gmail.com>
2025-10-05 18:48:38 +0600

    spread operator-1

---
+++ Spread Operator-1.js
@@ -0,0 +1,15 @@
+//Array Copy 
+const numbers=[3,5,2];
+const newNumbers=[numbers];
+console.log(newNumbers)
+
+
+
+
+//Array Merge 
+const num1=[1,2,3];
+const num2=[4,5,6];
+
+const combined=[...num1, ...num2];
+console.log(combined);
+
